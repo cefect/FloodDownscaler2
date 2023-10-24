1f919104e93bf4803c624a3f779baa38bdfc747c
cefect <cefect@gmail.com>
2023-10-24 17:58:07 +0200

    add runners

--- fdsc/main.py
+++ fdsc/main.py
@@ -5,12 +5,12 @@ Created on Oct. 24, 2023
 
 CLI caller for running downscalers
 '''
-import os
+import os, argparse
 from fdsc.control import Dsc_Session
 
 def downscale(
-    dem1_fp,
-    wse2_fp,            
+    dem_fp,
+    wse_fp,            
     
     method='CostGrow', 
     write_meta=True,
@@ -22,10 +22,10 @@ def downscale(
         
         Pars
         ----------
-        wse2_fp: str
+        wse_fp: str
             filepath to WSE raster layer at low-resolution (to be downscaled)
             
-        dem1_fp: str
+        dem_fp: str
             filepath to DEM raster layer at high-resolution (used to infer downscaled WSE)
             
         method: str
@@ -44,9 +44,37 @@ def downscale(
             
         with Dsc_Session(run_name='fdsc2', relative=True, out_dir=out_dir) as ses:
  
-            ses.run_dsc(dem1_fp, wse2_fp, method=method, write_meta=write_meta,
+            ses.run_dsc(dem_fp, wse_fp, method=method, write_meta=write_meta,
                         rkwargs=kwargs,
                         )
             
-            
  
+
+ 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Downscale a coarse WSE grid.')
+    parser.add_argument('dem_fp', type=str, help='Filepath to DEM raster layer at fine-resolution.')
+    parser.add_argument('wse_fp', type=str, help='Filepath to WSE raster layer at coarse-resolution.')
+    parser.add_argument('--method', type=str, default='CostGrow', help='Downscaling method to apply.')
+    parser.add_argument('--write_meta', type=bool, default=True, help='Flag to write metadata.')
+    parser.add_argument('--out_dir', type=str, default=None, help='Output directory.')
+    args, unknown_args = parser.parse_known_args()
+    
+    # parse unknowns
+    kwargs = dict()
+    k = None
+    for e in unknown_args:
+        # keys
+        if e.startswith('-'):
+            k = e.replace('-', '')
+            kwargs[k] = None
+            
+        # values
+        else:
+            assert not k is None
+            kwargs[k] = e
+            k = None
+
+    raise IOError('need to clean up the outputs')
+    downscale(args.dem_fp, args.wse_fp, method=args.method, write_meta=args.write_meta, out_dir=args.out_dir, **kwargs)
