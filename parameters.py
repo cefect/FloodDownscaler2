248bce44be43a4c6a9eefd343ab66be05e74f26a
Seth Bryant <bryant.seth@gmail.com>
2024-01-12 16:03:43 -0700

    pars note

--- parameters.py
+++ parameters.py
@@ -1,7 +1,10 @@
+"""WARNING: this should be superceeded if FlooDownscaler2 is added as a submodule"""
 import os
 
+
+
 src_dir = os.path.dirname(os.path.abspath(__file__))
-#src_name = os.path.basename(src_dir)
+src_name = os.path.basename(src_dir)
 
 #location of logging configuration file
 logcfg_file=os.path.join(src_dir, r'hp\logger.conf')
