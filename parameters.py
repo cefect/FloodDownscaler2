be1418790010a732947ae086f51b4558e04337cc
Seth Bryant <bryant.seth@gmail.com>
2024-05-25 15:27:02 +0100

    re-org and restart architecture

--- parameters.py
+++ parameters.py
@@ -1,7 +1,8 @@
 """WARNING: this should be superceeded if FlooDownscaler2 is added as a submodule"""
 import os
 
-
+import logging
+log_level = logging.DEBUG
 
 src_dir = os.path.dirname(os.path.abspath(__file__))
 src_name = os.path.basename(src_dir)
---
+++ tests/xxxtest_costGrow.py
@@ -0,0 +1,5 @@
+'''
+Created on May 25, 2024
+
+@author: cef
+'''
